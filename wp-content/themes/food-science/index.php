6cbd24a71b705e0da670af1329746101fdb0246b
tarosjk <tarosjk@gmail.com>
2024-08-22 11:19:13 +0900

    カテゴリー、年別一覧リンクを実装

--- wp-content/themes/food-science/index.php
+++ wp-content/themes/food-science/index.php
@@ -4,23 +4,31 @@
   <section class="section">
     <div class="section_inner">
       <div class="section_header">
-        <h1 class="heading heading-primary"><span>最新情報</span>NEWS - <?php wp_title(''); ?></h1>
+        <h1 class="heading heading-primary"><span>最新情報</span>NEWS - <?php wp_title(''); ?><?php if (is_year()): ?>年<?php endif; ?></h1>
       </div>
 
       <div class="archive">
         <div class="archive_category">
           <h2 class="archive_title">カテゴリー</h2>
           <ul class="archive_list">
-            <li class="current-cat"><a href="#">お知らせ</a></li>
-            <li><a href="#">コラム</a></li>
+            <?php
+            $args = [
+              'title_li' => '',
+            ];
+            wp_list_categories($args);
+            ?>
           </ul>
         </div>
 
         <div class="archive_yealy">
           <h2 class="archive_title">年別</h2>
           <ul class="archive_list">
-            <li><a href='#'>2023</a></li>
-            <li><a href='#'>2022</a></li>
+            <?php
+            $args = [
+              'type' => 'yearly',
+            ];
+            wp_get_archives($args);
+            ?>
           </ul>
         </div>
       </div>
