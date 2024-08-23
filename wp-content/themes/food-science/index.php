e0a10b39bda1ad7bb2eacac73810161d4d01e57a
tarosjk <tarosjk@gmail.com>
2024-08-23 14:44:42 +0900

    ページネーション追加（WP-Pagenavi）

--- wp-content/themes/food-science/index.php
+++ wp-content/themes/food-science/index.php
@@ -45,6 +45,15 @@
 
           </div>
         <?php endif; ?>
+
+        <?php
+        if (function_exists('wp_pagenavi')):
+        ?>
+          <div class="pagination">
+            <?php wp_pagenavi(); ?>
+          </div>
+        <?php endif; ?>
+
       </div>
 
     </div>
