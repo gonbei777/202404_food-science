e3f262b1a3054cad3cc119fa8ef8a58dce874c66
tarosjk <tarosjk@gmail.com>
2024-08-23 14:00:17 +0900

    Contact Form 7のPタグ削除を追加

--- wp-content/themes/food-science/functions.php
+++ wp-content/themes/food-science/functions.php
@@ -17,3 +17,11 @@ function my_document_title_separator($separator)
   $separator = '|';
   return $separator;
 }
+
+
+// Contact Form 7の出力の際にPタグが勝手に入らないようにする
+add_filter('wpcf7_autop_or_not', 'my_wpcf7_autop');
+function my_wpcf7_autop()
+{
+  return false;
+}
