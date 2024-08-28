34bdf166656b27a8da3be61801f8ac785ec23b16
tarosjk <tarosjk@gmail.com>
2024-08-28 11:15:05 +0900

    TOPページのメインビジュアルをカスタム投稿タイプとACFで管理に変更

--- wp-content/themes/food-science/front-page.php
+++ wp-content/themes/food-science/front-page.php
@@ -7,11 +7,26 @@
       <p class="kv_subtitle">FROM JAPAN</p>
     </div>
 
-    <div class="kv_slider js-slider">
-      <div class="kv_sliderItem" style="background-image: url('<?= get_template_directory_uri() ?>/assets/img/home/kv-01@2x.jpg');"></div>
-      <div class="kv_sliderItem" style="background-image: url('<?= get_template_directory_uri() ?>/assets/img/home/kv-02@2x.jpg');"></div>
-      <div class="kv_sliderItem" style="background-image: url('<?= get_template_directory_uri() ?>/assets/img/home/kv-03@2x.jpg');"></div>
-    </div>
+    <?php
+    $args = [
+      'post_type' => 'main-visual',
+      'posts_per_page' => -1,
+    ];
+    $the_query = new WP_Query($args);
+    ?>
+    <?php if ($the_query->have_posts()): ?>
+      <div class="kv_slider js-slider">
+        <?php while ($the_query->have_posts()):  $the_query->the_post(); ?>
+          <?php
+          $pic = get_field('pic');
+          ?>
+          <div class="kv_sliderItem" style="background-image: url('<?= $pic['url']; ?>');"></div>
+        <?php endwhile;
+        wp_reset_postdata();
+        ?>
+      </div>
+    <?php endif; ?>
+
     <div class="kv_overlay"></div>
 
     <div class="kv_scroll">
